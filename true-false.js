847c0acbc8d5eb8340c1ac274ca39920dc5b154e
ajoy100 <ajoy221098@gmail.com>
2020-03-08 14:00:54 +0600

    True Vs False

---
+++ true-false.js
@@ -0,0 +1,19 @@
+// const age=1;
+// if(age>0){
+//     console.log("Condition is True");
+// }
+// else{
+//     console.log("Condition is False");
+
+// }
+let name="AA";
+if(name){
+    console.log("Condition is True");
+}
+else{
+    console.log("Condition is False");
+
+}
+
+// Falsy : ( False , 0 , "" , undefined= ullekh na kora , null , NaN )
+// Truthy : opposite ('False' , "0"," ", etc )
