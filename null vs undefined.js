73426153c0fe9ee8de98e5f45084bed8bf53c185
ajoy100 <ajoy221098@gmail.com>
2020-03-08 14:42:59 +0600

    Null Vs Undefined

---
+++ null vs undefined.js
@@ -0,0 +1,25 @@
+let pakhi;
+console.log(pakhi);
+
+
+
+let ages=[14,11,15];
+console.log(ages[11]);
+
+
+
+function add(num1,num2){
+    console.log(num1+num2);  // Reuturn Na Dile Undefined Dibe 
+     return ; // Only Return Dile Undefined Dibe  , Declare Korte Hobe 
+}
+const result=add(14,16);
+//const result=add(15);  BackUp Na dile Undefined Dibe 
+console.log(result);
+
+
+
+const premik={name:"Smart Dude" , phone:01780};
+console.log(premik.gf); // Uncommon Property Dile Undefined Dibe 
+
+let fun=undefined; // Only Undefined Dilew Undefined Dibe 
+console.log(fun);
