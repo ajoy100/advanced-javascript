3ffe68a4940803ce3b91b440d977bc5b9800adf0
ajoy100 <ajoy221098@gmail.com>
2020-03-08 20:50:29 +0600

    Array Split

---
+++ array-split.js
@@ -0,0 +1,14 @@
+const nums=[1,2,3,4,5,6,7,8];
+//const part=nums.slice(1,5); // Slice (Copy + ALada ) Korle Mool Array Thik Thake 
+// start index & end Index
+
+//const removed=nums.splice(1,4); // Moll Array Soto Hoiye Jai 
+// Start index , kotogolo Delete seta 
+
+const removed=nums.splice(1,3,77,88,100); // porer golo insert ,,,Jotogolo Essa 
+//console.log(part);
+//console.log(removed);
+//console.log(nums);
+
+const join=nums.join(" ami "); // " , " // "" // " Something"
+console.log(join);
