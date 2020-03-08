d682faa05f7b2a7d4ba7dcd3f7d49788c496ddef
ajoy100 <ajoy221098@gmail.com>
2020-03-08 15:04:33 +0600

    == Vs ===

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,14 @@
+// const first=false;
+// const second=0;
+
+const first=1;
+const second='1';
+if(first===second){           
+                        // == Dile Type Check Korena  
+                        // === Dile Type Ckeck Kore 
+    console.log("Condition is True");
+}
+else{
+    console.log("Condition is False");
+
+}
